3410f5bce4145a4baf5fdb09ea48cefa8a21aa74
ngaask <116688427+ngaask@users.noreply.github.com>
2023-12-02 13:18:25 -0500

    Update MergeSort.h

--- SortingVisualizer/MergeSort.h
+++ SortingVisualizer/MergeSort.h
@@ -5,7 +5,6 @@ class MergeSort : public SortingAlgorithm {
 private:
 	void merge(float* pops, std::array<std::string, 1000> &names, int left, int mid, int right);
 	void merge_sort(float* pops, std::array<std::string, 1000> &names, int left, int right);
-	void push(std::array<std::string, 1000> names);
 public:
 	MergeSort(float* pops, std::array<std::string, 1000> &names);
 };
