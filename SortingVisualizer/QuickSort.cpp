4765fd22e36f06496254807d534242df82121ae9
Joey Perry <josephperry@ufl.edu>
2023-12-03 13:44:00 -0500

    Added comments to these 3 files.

--- SortingVisualizer/QuickSort.cpp
+++ SortingVisualizer/QuickSort.cpp
@@ -1,27 +1,27 @@
 #include "QuickSort.h"
 
-//Sorting code structure borrowed from module 6 lecture slides
+//Sorting code structure borrowed from module 6 lecture slides.
 
 QuickSort::QuickSort(float* input, std::array<std::string, 1000> names) {
-    Update(input, names); //Records initial state of array
-    std::chrono::system_clock::time_point start = std::chrono::system_clock::now(); //Records starting time
-    Sort(input, names, 0, 999); //Preforms quick sort
-    std::chrono::system_clock::time_point end = std::chrono::system_clock::now(); //records finishing time
-    sortTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start); //records duration sorting took
+    Update(input, names); //Records initial state of array.
+    std::chrono::system_clock::time_point start = std::chrono::system_clock::now(); //Records starting time.
+    Sort(input, names, 0, 999); //Preforms quick sort.
+    std::chrono::system_clock::time_point end = std::chrono::system_clock::now(); //Records finishing time.
+    sortTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start); //Records the duration the sorting took to complete.
 }
 
 void QuickSort::Sort(float *input, std::array<std::string, 1000> &names, int low, int high) {
     if(low < high){ //Unless the base case of the low integer now exceeding the high, a quick sort pass is conducted and split to be preformed again recursively.
-        int pivot = Partition(input, names, low, high); //Sort pass
-        Sort(input, names, low, pivot - 1); //Recursively sorts lower section
-        Sort(input, names, pivot + 1, high); //Recursively sorts higher section
+        int pivot = Partition(input, names, low, high); //Sort pass.
+        Sort(input, names, low, pivot - 1); //Recursively sorts lower section.
+        Sort(input, names, pivot + 1, high); //Recursively sorts higher section.
     }
 }
 
 int QuickSort::Partition(float* input, std::array<std::string, 1000> &names, int low, int high) {
-    float pivot = input[low];
-    int up = low;
-    int down = high;
+    float pivot = input[low]; //Initializes the pivot value to the first value of this section.
+    int up = low; //Initializes the up index to the start of this section.
+    int down = high; //Initializes the down index to the end of this section.
     while(up < down){
         for(int i = up; i < high; i++){ //Increments up until it finds a value greater than the pivot, or it has reached the end of this section.
             if(input[up] > pivot){
@@ -43,15 +43,15 @@ int QuickSort::Partition(float* input, std::array<std::string, 1000> &names, int
     return down;
 }
 
-void QuickSort::Swap(float* input, std::array<std::string, 1000> &names, int first, int second) { //Swaps two indices of the array
-    swapCount++; //Increments the number of swaps preformed
-    float temp = input[first];
-    std::string tempStr = names[first];
-    input[first] = input[second];
+void QuickSort::Swap(float* input, std::array<std::string, 1000> &names, int first, int second) { //Swaps two indices of the array.
+    swapCount++; //Increments the number of swaps preformed.
+    float temp = input[first]; //Stores a temporary float for swapping.
+    std::string tempStr = names[first]; //Stores a temporary string for swapping.
+    input[first] = input[second]; //Swaps populations.
     input[second] = temp;
-    names[first] = names[second];
+    names[first] = names[second]; //Swaps city names.
     names[second] = tempStr;
-    Update(input, names); //Updates the steps vector.
+    Update(input, names); //Updates the soring steps vectors so the data at this point in time can be recorded.
 }
 
 void QuickSort::Update(float* input, const std::array<std::string, 1000>& names) { //Creates a copy of the current state of the data being sorted and adds it to the steps.
--- SortingVisualizer/SortingAlgorithm.h
+++ SortingVisualizer/SortingAlgorithm.h
@@ -8,23 +8,22 @@
 
 class SortingAlgorithm {
 protected:
-    std::vector<float*> sortingSteps;
-    std::vector<std::array<std::string, 1000> > namesSortingSteps;
-    int currentStep = -1;
-    float* copyFloatPointer(float* rhs);
-    int swapCount = 0;
-    std::chrono::duration<int,std::milli> sortTime;
+    std::vector<float*> sortingSteps; //Stores every iteration of the data as it is being sorted.
+    std::vector<std::array<std::string, 1000> > namesSortingSteps; //Stores every iteration of the names of the data as it is being sorted.
+    int currentStep = -1; //Stores the current step in the iterations of the sorting steps. Used to track frames in visualization.
+    float* copyFloatPointer(float* rhs); //Creates a copy of a float array pointer. Used to make sure arrays in sorting steps are not the same object still being edited.
+    int swapCount = 0; //Keeps a count of how many swaps were preformed in the sorting algorithm.
+    std::chrono::duration<int,std::milli> sortTime; //Stores the real time it took for the sorting algorithm to execute in milliseconds.
 
 public:
-    std::pair<float*, std::array<std::string, 1000> > getNextStep();
-    std::pair<float*, std::array<std::string, 1000> > getPreviousStep();
-    bool areNoStepsRemaining();
-    bool isAtStart();
+    std::pair<float*, std::array<std::string, 1000> > getNextStep(); //Returns the arrays for the next step.
+    std::pair<float*, std::array<std::string, 1000> > getPreviousStep(); //Returns the arrays for the previous step.
+    bool areNoStepsRemaining(); //Checks if the last step is reached.
+    bool isAtStart(); //Checks if the current step is the first step.
     std::pair<float*, std::array<std::string, 1000> > getSorted();
-    void reset();
-    int getSwapCount();
-    std::chrono::duration<int,std::milli> getSortTime();
+    void reset(); //Resets the current step.
+    int getSwapCount(); //Getter function for the swap count.
+    std::chrono::duration<int,std::milli> getSortTime(); //Getter function for the sort time.
 
-
-    ~SortingAlgorithm();
+    ~SortingAlgorithm(); //Class destructor
 };
