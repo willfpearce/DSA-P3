f8e4ff89e6e7f86eab5a9a9f6c86581fdfe6fe94
ngaask <116688427+ngaask@users.noreply.github.com>
2023-12-04 21:49:37 -0500

    Added comments to MergeSort.cpp

--- SortingVisualizer/MergeSort.cpp
+++ SortingVisualizer/MergeSort.cpp
@@ -3,10 +3,12 @@
 void MergeSort::merge(float* pops, std::array<std::string, 1000> &names, int left, int mid, int right) {
 	int arr_1 = mid - left + 1;
 	int arr_2 = right - mid;
-	
+
+	// create two subarrays
 	std::array<std::pair<std::string, int>, 1000> left_arr;
 	std::array<std::pair<std::string, int>, 1000> right_arr;
-	
+
+	// copy data to the subarrays
 	for (int i = 0; i < arr_1; ++i) {
 		left_arr[i].first = names[left + i];
 		left_arr[i].second = pops[left + i];
@@ -22,6 +24,7 @@ void MergeSort::merge(float* pops, std::array<std::string, 1000> &names, int lef
 	int index_merged_arr = left;
 
 	while (index_arr_1 < arr_1 && index_arr_2 < arr_2) {
+		// compares the population number of the subarrays
 		if (left_arr[index_arr_1].second <= right_arr[index_arr_2].second) {
 			names[index_merged_arr] = left_arr[index_arr_1].first;
 			pops[index_merged_arr] = left_arr[index_arr_1].second;
@@ -35,7 +38,8 @@ void MergeSort::merge(float* pops, std::array<std::string, 1000> &names, int lef
 		++index_merged_arr;
 		++swapCount;
 	}
-	
+
+	// copy any elements left, if there are any
 	while (index_arr_1 < arr_1) {
 		names[index_merged_arr] = left_arr[index_arr_1].first;
 		pops[index_merged_arr] = left_arr[index_arr_1].second;
@@ -52,6 +56,7 @@ void MergeSort::merge(float* pops, std::array<std::string, 1000> &names, int lef
 		++swapCount;
 	}
 
+	// push the copy of each change after the merge for the visualization purposes
 	auto copy = copyFloatPointer(pops);
 	sortingSteps.push_back(copy);
 	auto copy_2 = names;
@@ -60,6 +65,7 @@ void MergeSort::merge(float* pops, std::array<std::string, 1000> &names, int lef
 }
 
 void MergeSort::merge_sort(float* pops, std::array<std::string, 1000> &names, int left, int right) {
+	// condition to check whether the parameters are valid
 	if (left < right) {
 		int mid = left + (right - left) / 2;
 		merge_sort(pops, names, left, mid);
@@ -69,13 +75,16 @@ void MergeSort::merge_sort(float* pops, std::array<std::string, 1000> &names, in
 }
 
 MergeSort::MergeSort(float* pops, std::array<std::string, 1000> &names) {
+	// push the initial unordered copy of the input (sortingSteps and namesSortingSteps are class member variables of the SortingAlgorithm class)
 	auto copy = copyFloatPointer(pops);
 	sortingSteps.push_back(copy);
 	auto copy_2 = names;
 	namesSortingSteps.push_back(copy_2);
+	// used to track the time it takes for the algorithm to fully sort the input
 	std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
 	merge_sort(pops, names, 0, 999);
 	std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
+	// class member variable of the SortingAlgorithm class
 	sortTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 }
 
