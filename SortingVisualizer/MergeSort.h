1af7468d5855c56d91b9e6d07f7c57e8549b909f
ngaask <116688427+ngaask@users.noreply.github.com>
2023-12-04 21:26:12 -0500

    Adding comments to MergeSort.h

--- SortingVisualizer/MergeSort.h
+++ SortingVisualizer/MergeSort.h
@@ -3,8 +3,8 @@
 
 class MergeSort : public SortingAlgorithm {
 private:
-	void merge(float* pops, std::array<std::string, 1000> &names, int left, int mid, int right);
-	void merge_sort(float* pops, std::array<std::string, 1000> &names, int left, int right);
+	void merge(float* pops, std::array<std::string, 1000> &names, int left, int mid, int right); // merging the subarrays (helper function)
+	void merge_sort(float* pops, std::array<std::string, 1000> &names, int left, int right); // the algorithm itself
 public:
-	MergeSort(float* pops, std::array<std::string, 1000> &names);
+	MergeSort(float* pops, std::array<std::string, 1000> &names); // constructor that ties the algorithm to the structure of the project
 };
